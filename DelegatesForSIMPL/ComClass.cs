32f51a2382e4ad5619b20d5f5019c5d8bcc5c6f3
Panos-AI <91608320+Panos-AI@users.noreply.github.com>
2022-10-03 00:01:29 +0300

    Add project files.

---
+++ DelegatesForSIMPL/ComClass.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Text;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+
+namespace DelegatesForSIMPL
+{
+    public class ComClass
+    {
+
+
+
+        public delegate void myDelegate(ushort data, SimplSharpString txt);
+        public myDelegate myEvent { get; set; }
+
+
+
+
+        static private Random i = new Random();
+      
+     
+
+        public void func1()
+        {
+            
+
+            ushort myInt;
+   
+
+            myInt = (UInt16)i.Next(1, 32000);
+            myEvent(Convert.ToUInt16(myInt), "The number is " + Convert.ToString(myInt));
+        }
+    }
+}
+
